fe0fe396a81f770f78f366a773c59ebc81754ded
Fabien Potencier <fabien.potencier@gmail.com>
2013-03-24 15:07:44 +0100

    added missing LICENSE information

--- SensioLabs/Security/Command/SecurityCheckerCommand.php
+++ SensioLabs/Security/Command/SecurityCheckerCommand.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the SensioLabs Security Checker.
+ *
+ * (c) 2013 Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SensioLabs\Security\Command;
 
 use SensioLabs\Security\SecurityChecker;
