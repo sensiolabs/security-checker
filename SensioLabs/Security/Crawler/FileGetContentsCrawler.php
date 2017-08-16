ee474f299dd0097af010c1b7ce49d82222b57eb6
David Arenas <david.arenas@worldfirst.com>
2017-08-16 15:13:15 +0100

    Fix setting certificate locations

--- SensioLabs/Security/Crawler/FileGetContentsCrawler.php
+++ SensioLabs/Security/Crawler/FileGetContentsCrawler.php
@@ -34,7 +34,8 @@ class FileGetContentsCrawler extends BaseCrawler
         foreach ($contextualHeaders as $key => $value) {
             $headers .= "\r\n$key: $value";
         }
-        $context = stream_context_create(array(
+
+        $opts = array(
             'http' => array(
                 'method' => 'POST',
                 'header' => $headers,
@@ -46,12 +47,19 @@ class FileGetContentsCrawler extends BaseCrawler
                 'user_agent' => sprintf('SecurityChecker-CLI/%s FGC PHP', SecurityChecker::VERSION),
             ),
             'ssl' => array(
-                'cafile' => CaBundle::getSystemCaRootBundlePath(),
                 'verify_peer' => 1,
                 'verify_host' => 2,
             ),
-        ));
+        );
 
+        $caPathOrFile = CaBundle::getSystemCaRootBundlePath();
+        if (is_dir($caPathOrFile) || (is_link($caPathOrFile) && is_dir(readlink($caPathOrFile)))) {
+            $opts['ssl']['capath'] = $caPathOrFile;
+        } else {
+            $opts['ssl']['cafile'] = $caPathOrFile;
+        }
+
+        $context = stream_context_create($opts);
         $level = error_reporting(0);
         $body = file_get_contents($this->endPoint, 0, $context);
         error_reporting($level);
